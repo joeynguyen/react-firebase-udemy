2e435f0333e978137c2c77659e96c61840ebf467
Joey Nguyen <jnn193@gmail.com>
2015-10-18 02:02:40 -0500

    Disable the field if the todo is checked, marking it as done.

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -24,6 +24,7 @@ module.exports = React.createClass({
                     </span>
                     <input
                         type="text"
+                        disabled={this.state.done}
                         className="form-control"
                         onChange={this.handleEditText}
                         value={this.state.text}
