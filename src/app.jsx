d294d74521eb9135aab386deb7e52dc08b6239b6
Joey Nguyen <jnn193@gmail.com>
2015-10-17 23:04:54 -0500

    Added function to handle loaded state of app after Firebase data has been received.

--- src/app.jsx
+++ src/app.jsx
@@ -9,23 +9,29 @@ var App = React.createClass({
     mixins: [ ReactFire ],
     getInitialState: function(){
         return {
-            items: {}
+            items: {},
+            loaded: false
         }
     },
     componentWillMount: function() {
-        this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
-        // shortcut for:
-        // this.firebase = new Firebase(rootUrl + 'items/');
-        // this.bindAsObject(this.firebase, 'items');
+        var firebase = new Firebase(rootUrl + 'items/');
+        this.bindAsObject(firebase, 'items');
+        // FireBase has an event called 'value' for when it sees data flow in from server
+        firebase.on('value', this.handleDataLoaded);
     },
     render: function() {
         return <div className="row panel panel-default">
             <div className="col-md-8 col-md-offset-2">
                 <h2 className="text-center">To-Do List</h2>
                 <Header itemsStore={this.firebaseRefs.items} />
-                <List items={this.state.items} />
+                <div className={"content " + (this.state.loaded ? 'loaded':'')}>
+                    <List items={this.state.items} />
+                </div>
             </div>
         </div>
+    },
+    handleDataLoaded: function() {
+        this.setState({ loaded: true })
     }
 });
 
