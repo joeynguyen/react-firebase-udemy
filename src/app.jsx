f21726fed4c1a08be3fa1a5cbb8f0f6c8240fc8e
Joey Nguyen <jnn193@gmail.com>
2015-10-17 22:32:03 -0500

    Created list component to output data from Firebase.

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,16 @@ var React = require('react'),
     ReactFire = require('reactfire'),
     Firebase = require('firebase'),
     Header = require('./header'),
+    List = require('./list'),
     rootUrl = 'https://glaring-fire-1823.firebaseio.com/';
 
 var App = React.createClass({
     mixins: [ ReactFire ],
+    getInitialState: function(){
+        return {
+            items: {}
+        }
+    },
     componentWillMount: function() {
         this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
         // shortcut for:
@@ -17,6 +23,7 @@ var App = React.createClass({
             <div className="col-md-8 col-md-offset-2">
                 <h2 className="text-center">To-Do List</h2>
                 <Header itemsStore={this.firebaseRefs.items} />
+                <List items={this.state.items} />
             </div>
         </div>
     }
---
+++ src/list.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function() {
+        return <ul>
+            {this.renderList()}
+        </ul>
+    },
+    renderList: function() {
+        if (this.props.items && Object.keys(this.props.items).length === 0) {
+            return <h2>
+                Add a todo to get started.
+            </h2>
+        } else {
+            var children = [];
+            for (var key in this.props.items) {
+                children.push(
+                    <li>{this.props.items[key].text}</li>
+                )
+            }
+            return children;
+        }
+    }
+});
